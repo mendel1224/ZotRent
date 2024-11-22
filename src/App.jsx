1a4512677f3ed9a1b08dd83aac47544ebdf15814
mendel1224 <75659972+mendel1224@users.noreply.github.com>
2024-11-22 10:26:29 -0800

    Linked rental page to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,44 @@
 import React from 'react';
-import HeroSection from './components/AppComponents/hero';
-import Button from '@mui/material/Button';
-import AppBar from '@mui/material/AppBar';
-import { Toolbar, Typography, Box } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import Header from './components/AppComponents/header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, CssBaseline } from '@mui/material';
 import theme from './components/AppComponents/theme';
+
+// Import your components
+import Header from './components/AppComponents/header';
+import HeroSection from './components/AppComponents/hero';
 import MapSection from './components/AppComponents/mapSection';
 import FeaturedListings from './components/AppComponents/featuredListings';
-
+import FullApartment from './components/AppComponents/fullApartment';
+import ThankYouPage from './components/AppComponents/ThankYouListing';
 
 
 function App() {
   return (
-    
     <ThemeProvider theme={theme}>
-    <CssBaseline />
+      <CssBaseline />
+      
+      <Router>
+        <Header />
+        <Routes>
+          {/* Home Page */}
+          <Route
+            path="/"
+            element={
+              <>
+                <HeroSection />
+                <MapSection />
+                <FeaturedListings />
+              </>
+            }
+          />
 
-    <Header />
-    <HeroSection />
-    <MapSection />
-    <FeaturedListings/>
+          {/*Apartment Listing page */}
+          <Route path="/list-property" element={<FullApartment />} />
 
+          {/* Thank you page*/}
+          <Route path="/thank-you" element={<ThankYouPage />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
